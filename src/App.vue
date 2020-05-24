10a5bab596f72f69bf67717711d76e4d7cea50e3
devmobin <themobinamiri@gmail.com>
2020-05-24 21:09:49 +0430

    auto format

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,15 @@
 </template>
 
 <script>
-import Header from "./components/Header";
+import Header from './components/Header';
 
 export default {
   components: {
-    Header
+    Header,
   },
   created() {
-    this.$store.dispatch("initStocks");
-  }
+    this.$store.dispatch('initStocks');
+  },
 };
 </script>
 
