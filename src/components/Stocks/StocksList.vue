b7d72c58cf6a982729dda9ef9230c1ecb2672391
devmobin <themobinamiri@gmail.com>
2020-05-24 18:57:58 +0430

    initial stocks page

--- src/components/Stocks/StocksList.vue
+++ src/components/Stocks/StocksList.vue
@@ -1,13 +1,41 @@
 <template>
-  <h1>Stock List Component</h1>
+  <Stock v-for="stock in stocks" :stock="stock"></Stock>
 </template>
 
 <script>
-export default {
+import Stock from './Stock';
 
-}
+export default {
+  data() {
+    return {
+      stocks: [
+        {
+          id: 1,
+          name: 'BMW',
+          price: 110,
+        },
+        {
+          id: 2,
+          name: 'Google',
+          price: 200,
+        },
+        {
+          id: 3,
+          name: 'Apple',
+          price: 250,
+        },
+        {
+          id: 4,
+          name: 'Twitter',
+          price: 8,
+        },
+      ],
+    };
+  },
+  components: {
+    Stock,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
