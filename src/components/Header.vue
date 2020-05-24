322af471c85daaec2a538e7c92ed076c91b8b72b
devmobin <themobinamiri@gmail.com>
2020-05-24 22:50:29 +0430

    format the file and end day

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,18 +7,18 @@
 
       <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
-          <router-link to="/portfolio" activeClass="active" tag="li">
-            <a>Portfolio</a>
-          </router-link>
-          <router-link to="/stocks" activeClass="active" tag="li">
-            <a>Stocks</a>
-          </router-link>
+          <router-link to="/portfolio" activeClass="active" tag="li"
+            ><a>Portfolio</a></router-link
+          >
+          <router-link to="/stocks" activeClass="active" tag="li"
+            ><a>Stocks</a></router-link
+          >
         </ul>
-        <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
+        <strong class="navbar-text navbar-right"
+          >Funds: {{ funds | currency }}</strong
+        >
         <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a href="#" @click="endDay">End Day</a>
-          </li>
+          <li><a href="#" @click="endDay">End Day</a></li>
           <li
             class="dropdown"
             :class="{ open: isDropdownOpen }"
@@ -31,17 +31,11 @@
               role="button"
               aria-haspopup="true"
               aria-expanded="false"
-            >
-              Save & Load
-              <span class="caret"></span>
-            </a>
+              >Save & Load <span class="caret"></span
+            ></a>
             <ul class="dropdown-menu">
-              <li>
-                <a href="#" @click="saveData">Save Data</a>
-              </li>
-              <li>
-                <a href="#" @click="loadData">Load Data</a>
-              </li>
+              <li><a href="#" @click="saveData">Save Data</a></li>
+              <li><a href="#" @click="loadData">Load Data</a></li>
             </ul>
           </li>
         </ul>
@@ -53,12 +47,25 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
+  data() {
+    return {};
+  },
   computed: {
     funds() {
       return this.$store.getters.funds;
-    }
-  }
+    },
+  },
+  methods: {
+    ...mapActions({
+      randomizeStocks: 'randomizeStocks',
+    }),
+    endDay() {
+      this.randomizeStocks();
+    },
+  },
 };
 </script>
 
